f9ef619086e0f8c13f743c1da25264eb47ce5afa
al-abir-anik <alabiranik56@gmail.com>
2024-12-25 05:07:36 +0600

    Get Crud operation added to load all data and single data

--- index.js
+++ index.js
@@ -2,16 +2,15 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require("mongodb");
 
+// Middlewires
 app.use(cors());
 app.use(express.json());
 
-// USer:
-// PASS : AdggX5zAVvOoOYFP
-
-const uri = `mongodb+srv://alabiranik56:<db_password>@foodbridge-cluster.cxkdi.mongodb.net/?retryWrites=true&w=majority&appName=FoodBridge-Cluster`;
+// CONNECTION CODE START___
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@foodbridge-cluster.cxkdi.mongodb.net/?retryWrites=true&w=majority&appName=FoodBridge-Cluster`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -31,9 +30,37 @@ async function run() {
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
+
+    // Food Related APIs...............
+    const foodCollection = client
+      .db("FoodBridge")
+      .collection("food_collection");
+
+    // To load all foods
+    app.get("/foods", async (req, res) => {
+      const cursor = foodCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    // To get specific Food
+    app.get("/foods/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await foodCollection.findOne(query);
+      res.send(result);
+    });
+    // Add a new Movie
+    app.post("/foods", async (req, res) => {
+      const newFood = req.body;
+      const result = await foodCollection.insertOne(newFood);
+      res.send(result);
+    });
+
+
+
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
